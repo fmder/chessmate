1d7766511cd6abb3d6fdd9ba9ae1bcf793648b60
François-Michel De Rainville <f.derainville@gmail.com>
2021-03-30 22:14:21 -0400

    Add test classes for engine features

---
+++ tests/test_features_engine.py
@@ -0,0 +1,15 @@
+import unittest
+
+import chess
+
+from chessmate.features import engine
+
+
+class TestRawMaterialScore(unittest.TestCase):
+    def test_fail(self):
+        raise NotImplementedError()
+
+
+class TestEngineEstimate(unittest.TestCase):
+    def test_fail(self):
+        raise NotImplementedError()
